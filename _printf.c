32a3333a8498582dc47d150b59b4b23d357439c6
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-23 17:18:45 -0700

    Fix % Errors

--- _printf.c
+++ _printf.c
@@ -3,16 +3,20 @@ int
 _printf(const char *format, ...)
 {
 	va_list args;
-	int len = 0, c = 0, x = 0, flag = 0;
+	int l = 0, c = 0, x = 0, flag = 0;
 
 	if (format == NULL)
 	{
 	return (-1);
 	}
 
-	len = strlen(format);
+	l = strlen(format);
 
-	if (format[len - 2] != '%' && format[len - 1] == '%')
+	if (format[l - 2] != '%' && format[l - 1] == '%')
+	{
+	flag = 1;
+	}
+	if (format[l - 3] == '%' && format[l - 2] == '%' && format[l - 1] == '%')
 	{
 	flag = 1;
 	}
