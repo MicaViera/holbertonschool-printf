32da34c1ceb053a24219fe4f36107911e1f6b6df
MicaViera <vieranoelia98@gmail.com>
2023-07-23 18:46:39 -0700

    This file contains all the protypes of the functions and the libraries.

--- main.h
+++ main.h
@@ -1,3 +1,8 @@
+#ifdef _MAIN_H_
+#define _MAIN_H_
+/**
+ * This file contains all the prototypes of the function and the libraries.
+ */
 #include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -9,3 +14,4 @@ int switchprint(const char *format, va_list args);
 int counter16(const char *format);
 char* my_itoa(long int num, char* str);
 void reverser(char *str);
+#endif
